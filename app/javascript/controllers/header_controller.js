36a3035ec5850664ed733e9502153981ca441bb5
rudy <c3tc3tc3t@126.com>
2026-08-20 13:46:02 +0800

    add menu toggle

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave, toggle } from "el-transition";
+
+export default class extends Controller {
+  static targets = ["openMenu", "dropDown"];
+
+  connect() {
+    this.openMenuTarget.addEventListener("click", (e) => {
+      openDropDown(this.dropDownTarget);
+    });
+  }
+}
+
+
+function openDropDown(element) {
+  toggle(element)
+}
