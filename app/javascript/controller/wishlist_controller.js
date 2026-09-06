4c25483ab64e5a91821636ddc9683187e221195e
rudy <c3tc3tc3t@126.com>
2026-09-06 20:47:33 +0800

    add sign_in validates for wishlist

--- app/javascript/controller/wishlist_controller.js
+++ app/javascript/controller/wishlist_controller.js
@@ -1,11 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
 
-  connect() {
-    console.log("wishlist")
+  updateWishlistStatus() {
+    const isUserLoggedIn = this.element.dataset.userLoggedIn;
+
+    if(isUserLoggedIn === "false"){
+      document.querySelector(".js-login").click()
+      return;
+    }
+
+    if (this.element.dataset.status === "false") {
+      this.element.classList.remove("fill-none");
+      this.element.classList.add("fill-red-500");
+      this.element.dataset.status = "true";
+    } else {
+      this.element.classList.remove("fill-red-500");
+      this.element.classList.add("fill-none");
+      this.element.dataset.status = "false";
+    }
   }
-
 }
-
-
