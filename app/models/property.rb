4be046e45ed7527ffb8c9d574ca5f1f20015e758
rudy <c3tc3tc3t@126.com>
2026-09-24 20:48:41 +0800

    add review overall_rating

--- app/models/property.rb
+++ app/models/property.rb
@@ -38,6 +38,21 @@ class Property < ApplicationRecord
   end
 
   def average_cleanliness_rating
-    reviews.average(:cleanliness_rating)
+    reviews.average(:cleanliness_rating).to_f
+  end
+  def average_accuracy_rating
+    reviews.average(:accuracy_rating).to_f
+  end
+  def average_checkin_rating
+    reviews.average(:checkin_rating).to_f
+  end
+  def average_communication_rating
+    reviews.average(:communication_rating).to_f
+  end
+  def average_value_rating
+    reviews.average(:value_rating).to_f
+  end
+  def average_location_rating
+    reviews.average(:location_rating).to_f
   end
 end
